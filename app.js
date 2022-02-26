bcd7d031f19a472d212187a7bb8fd5dd09ff2984
bomzaza <bombanlang2539@gmail.com>
2022-02-26 15:01:52 +0700

    Add All File

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+//const chalk = require("chalk")
+const debug = require("debug")
+const req = require("express/lib/request")
+const res = require("express/lib/response")
+const app  = express()
+const port = 3000
+app.get("/", (req,res)=>{
+
+    res.send('Hello Banlang')
+
+})
+
+app.listen(port, ()=>{
+    console.log("Listening on port "+ port)
+})
